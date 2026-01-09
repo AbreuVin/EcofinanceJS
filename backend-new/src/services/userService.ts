04e59d7390532490faeff620129be62a2f17d709
Vinícius Abreu <viniciusabreu115@gmail.com>
2026-01-08 21:04:15 -0300

    Added getLoggedUser

--- backend-new/src/services/userService.ts
+++ backend-new/src/services/userService.ts
@@ -1,8 +1,9 @@
-import { CreateUserDTO, UserPayload } from "../types/user";
+import { CreateUserDTO } from "../schemas/userSchema";
 import * as userRepository from "../repositories/userRepository";
 import { hashPassword } from "../utils/passwordUtils";
+import { AppJwtPayload } from "../types/auth";
 
-export const registerUser = async (userData: CreateUserDTO, currentUser: UserPayload) => {
+export const registerUser = async (userData: CreateUserDTO, currentUser: AppJwtPayload) => {
     const exists = await userRepository.findByEmail(userData.email);
     if (exists) throw new Error('User already exists');
 
@@ -15,4 +16,12 @@ export const registerUser = async (userData: CreateUserDTO, currentUser: UserPay
         companyId,
         parentId: currentUser.id
     });
+}
+
+export const fetchLoggedUser = async (userId: string) => {
+    const user = await userRepository.findById(userId);
+
+    if (!user) throw new Error('User not found');
+
+    return user;
 }
