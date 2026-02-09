4f75bb5f0a50aa4000c4148818109533fbc62596
luccamvmrey <luccamvmachadorey@gmail.com>
2026-02-09 19:27:02 -0300

    added foramtting to cnpj and phone

--- frontend-react/src/features/users/schemas/user.schema.ts
+++ frontend-react/src/features/users/schemas/user.schema.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 export const userFormSchema = z.object({
     name: z.string().min(1, "Nome é obrigatório."),
     email: z.string().email("E-mail inválido."),
-    phone: z.string().optional(),
+    phone: z.string()
+        .min(14, "Telefone incompleto")
+        .max(15, "Telefone inválido")
+        .optional(),
     role: z.enum(["MASTER", "ADMIN", "USER"], "Selecione um perfil válido."),
     companyId: z.string().optional(),
     unitId: z.coerce.number().optional(),
